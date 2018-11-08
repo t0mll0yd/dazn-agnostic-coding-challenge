7fa1ed5ffcb631971f8eb4e39f90de2ab7b47ca7
Tom Lloyd <thomas.lloyd@sky.uk>
2018-11-08 21:46:26 +0000

    Fixed InMemoryStore tests

--- src/test/scala/com/example/daznagnosticcodingchallenge/stores/InMemoryStoreSpec.scala
+++ src/test/scala/com/example/daznagnosticcodingchallenge/stores/InMemoryStoreSpec.scala
@@ -1,5 +1,6 @@
 package com.example.daznagnosticcodingchallenge.stores
 
+import com.twitter.util.Await
 import org.specs2.mutable.Specification
 
 
@@ -8,7 +9,41 @@ class InMemoryStoreSpec extends Specification {
     "Returns empty set if user has no streams" in {
       val store = InMemoryStore()
 
-      store.getStreams("1") must beEqualTo(Set())
+      val response = Await.result {
+        store.getStreams("userId")
+      }
+
+      response must beEqualTo(Set())
+    }
+
+    "Returns streams that have been added" in {
+      val store = InMemoryStore()
+
+      val response = Await.result {
+        for {
+          _ <- store.addStream("user1", "stream1")
+          _ <- store.addStream("user1", "stream2")
+          _ <- store.addStream("user1", "stream3")
+          streams <- store.getStreams("user1")
+        } yield streams
+      }
+
+      response must beEqualTo(Set("stream1", "stream2", "stream3"))
+    }
+
+    "Does not return streams for other users" in {
+      val store = InMemoryStore()
+
+      val response = Await.result {
+        for {
+          _ <- store.addStream("user1", "stream1")
+          _ <- store.addStream("user2", "stream2")
+          _ <- store.addStream("user3", "stream3")
+          streams <- store.getStreams("user1")
+        } yield streams
+      }
+
+      response must beEqualTo(Set("stream1"))
     }
   }
 }
